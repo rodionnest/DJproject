575310466f192a401a02bc7c8d024183c33140fd
Rodion Nest <rodionlxlnest@gmail.com>
2021-04-06 21:03:14 +0700

    Updated Models

--- project/NewsPaper/models.py
+++ project/NewsPaper/models.py
@@ -9,49 +9,61 @@ class Author(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     user_rating = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Автор'
         verbose_name_plural = 'Авторы'
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
-
-# class Post(models.Model):
-#     username = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.name
-
-#     class Meta:
-#         verbose_name = 'Пост'
-#         verbose_name_plural = 'Посты'
+    def __str__(self):
+        return self.name
 
 
-# class PostCategory(models.Model):
-#     name = models.CharField(max_length=255)
+class Post(models.Model):
+    category = models.ManyToManyField(Category, through=PostCategory)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=10,
+                                 choices=['article', 'news'], default='article')  # ! насколько это правильное решение? Или лучше реализовывать через кортежи?
+    datetime = models.DateTimeField(auto_now_add)
+    title = models.CharField(max_length=256)
+    text = models.TextField(blank=True)
+    rating = models.IntegerField(default=0)
 
-#     def __str__(self):
-#         return self.name
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.name
 
 
-# class Comment(models.Model):
-#     name = models.CharField(max_length=255)
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-#     class Meta:
-#         verbose_name = 'Комментарий'
-#         verbose_name_plural = 'Комментарии'
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    username = models.OneToMany(User, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    post_datetime = models.DateTimeField(auto_now_add)
+    comm_rating = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.name
