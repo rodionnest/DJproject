0f82f1746f0d7cff52ad5d13d8f45a874ae77a5f
Rodion Nest <rodionlxlnest@gmail.com>
2021-04-06 21:56:10 +0700

    Post and Comment admin register

--- project/NewsPaper/admin.py
+++ project/NewsPaper/admin.py
@@ -4,3 +4,5 @@ from NewsPaper.models import Author, Category
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
