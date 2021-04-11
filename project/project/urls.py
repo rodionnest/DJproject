26913142421468abaa35289bcf39d9b16a9ec70d
Rodion Nest <rodionlxlnest@gmail.com>
2021-04-11 19:52:19 +0700

    Added redirect, admin link, posttext fix in beaver

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from NewsPaper import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/news')),
     path('admin/', admin.site.urls),
     path('news/', include('NewsPaper.urls')),
+
 ]
