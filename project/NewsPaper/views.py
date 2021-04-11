aac94438e75f0ffe866b357639c4cb77ba889461
Rodion Nest <rodionlxlnest@gmail.com>
2021-04-11 11:25:43 +0700

    Added PostDetail, int:pk URL, post.html

--- project/NewsPaper/views.py
+++ project/NewsPaper/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,3 +7,9 @@ class PostList(ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
